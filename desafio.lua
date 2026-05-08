450657081d93de3d168e97c8e6195f61acf9a969
FABIO ALENCAR LIMA <falencarlima@gmail.com>
2026-05-07 21:36:31 -0400

    Update desafio.lua
    Foram acrescentadas mais duas funções para desenhar as bodas de cima e de baixo do card. Também foi feito o redimensionamento do card.

--- desafio.lua
+++ desafio.lua
@@ -19,7 +19,17 @@
 -- Habilitar UTF-8 no terminal e depois limpa a mensagem de código de página
 os.execute("chcp 65001")
 os.execute("cls")
--- Função responsável por gerar as barras de progresso
+-- Funções responsáveis pelas bordas de cima e de baixo do card.
+local function drawBorderTop()
+    print("▨▨▨▨▨▨▨▨▨▨▨▨▨ MONSTER CARD ▨▨▨▨▨▨▨▨▨▨▨▨▨")
+    return
+end
+local function drawBorderBottom()
+    print("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨")
+    return
+end
+
+-- Função responsável por gerar as barras de progresso.
 local function getProgressBar(attribute)
     local result = ""
     local fullChar = "▮"
@@ -34,29 +44,31 @@ local function getProgressBar(attribute)
     return result
 end
 
--- Descrição da criatura
+-- Descrição da criatura.
 local monsterName = "Zombie"
-local monsterDescription = "                                                   ▥▥\n▥▥   Criatura morta-viva muito hostil, causa dano corpo a corpo. ▥▥"
+local monsterDescription = [[
+▨    Criatura morta-viva muito hostil, ▨
+▨    causa dano corpo a corpo.         ▨]]
 local monsterSound = "grrruuuoooo"
 local monsterEmoji = "🧟"
 
--- Atributos da criatura
+-- Atributos da criatura.
 local attackAttribute = 3
 local defenseAttribute = 3
 local vitalityAttribute = 10
 local speedAttribute = 2
 local inteligenceAttribute = 1
 
---
-print("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨")
-print("▥▥ Nome da criatura: " .. monsterName .. "                                      ▥▥")
-print("▥▥ Emoji favorito: " .. monsterEmoji .. "                                            ▥▥")
-print("▥▥ Descrição: " .. monsterDescription)
-print("▥▥ Som que ele faz: " .. monsterSound .. "                                  ▥▥")
-print("▥▥ Attributos:" .. "                                                   ▥▥")
-print("▥▥          Atack: " .. getProgressBar(attackAttribute) .. "                                    ▥▥")
-print("▥▥         Defesa: " .. getProgressBar(defenseAttribute) .. "                                    ▥▥")
-print("▥▥           Vida: " .. getProgressBar(vitalityAttribute) .. "                                    ▥▥")
-print("▥▥     Velocidade: " .. getProgressBar(speedAttribute) .. "                                    ▥▥")
-print("▥▥   Inteligencia: " .. getProgressBar(inteligenceAttribute) .. "                                    ▥▥")
-print("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨")
+-- Card com informações da criatura.
+drawBorderTop()
+print("▨  Nome da criatura: " .. monsterName .. "            ▨")
+print("▨  Emoji favorito: " .. monsterEmoji .. "                  ▨")
+print("▨  Descrição:                          ▨\n" .. monsterDescription)
+print("▨  Som que ele faz: " .. monsterSound .. "        ▨")
+print("▨  Attributos:" .. "                         ▨")
+print("▨           Atack: " .. getProgressBar(attackAttribute) .. "          ▨")
+print("▨          Defesa: " .. getProgressBar(defenseAttribute) .. "          ▨")
+print("▨            Vida: " .. getProgressBar(vitalityAttribute) .. "          ▨")
+print("▨      Velocidade: " .. getProgressBar(speedAttribute) .. "          ▨")
+print("▨    Inteligencia: " .. getProgressBar(inteligenceAttribute) .. "          ▨")
+drawBorderBottom()
